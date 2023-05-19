83d2a61ada37cdc07615b7e71f9b3bc316e65224
webdevbynight <contact@victor-brito.name>
2023-05-19 23:11:02 +0200

    Styled menu on small screens when JS is activated

--- js/scripts.js
+++ js/scripts.js
@@ -45,6 +45,32 @@ if ('matchMedia' in window && 'querySelector' in document && typeof DOMTokenList
                 const logo = document.querySelector('#header h1 img');
                 logo.width = (window.matchMedia(`(min-width: ${getEmFromPx(mediaQueriesBreakpoints['horizontal-menu'])})`).matches)? '48' : '40';
                 logo.height = logo.width;
+                
+                // The menu show on small screens
+                const menu = document.getElementById('menu'),
+                    closeButton = menu.querySelector('.close-menu');
+                if (window.matchMedia(`(max-width: ${getEmFromPx(mediaQueriesBreakpoints['horizontal-menu'])})`).matches)
+                {
+                    const skipLinkMenu = document.querySelector('#skip-link a');
+                    if (!closeButton)
+                    {
+                        const button = document.createElement('button');
+                        button.setAttribute('type', 'button');
+                        button.classList.add('close-menu');
+                        button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="21"><desc>Close menu</desc><g><path d="M2.575.954l16.97 16.97-2.12 2.122L.455 3.076z"/><path d="M.454 17.925L17.424.955l2.122 2.12-16.97 16.97z"/></g></svg>';
+                        button.addEventListener('click', () =>
+                        {
+                            menu.classList.remove('active');
+                        });
+                        menu.appendChild(button);
+                    }
+                    skipLinkMenu.addEventListener('click', e =>
+                    {
+                        e.preventDefault();
+                        menu.classList.add('active');
+                    });
+                }
+                else if (closeButton) closeButton.remove();
             });
         });
     });
